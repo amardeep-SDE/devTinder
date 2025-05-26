5a664c208ee2645cd6ef87dab36a01169553aaf8
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-05-26 21:04:13 +0530

    next function and errors along with res.send()

--- src/app.js
+++ src/app.js
@@ -2,30 +2,36 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/user", (req, res) => {
-//     res.send("Hello, user!");
-// })
+// app.use("/route", rH1, [rH2, rH3, rH4], rH5);
 
-app.get("/user", (req, res) => {
-  res.send([{ name: "amardeep", age: 32 }, { name: "sachin", age: 30 }]);
-});
-
-app.post("/user", (req, res) => {
-  res.send({ message: "User created successfully!" });
-});
-app.delete("/user", (req, res) => {
-  res.send({ message: "User deleted successfully!" });
-});
-app.get("/user/:id", (req, res) => {
-    console.log(req.params.id);
-    
-    const userId = req.params.id;
-    res.send({ message: `User ID is ${userId}` });
-})
-
-app.use("/test", (req, res) => {
-  res.send("Hello, test!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("Hello, world 1!");
+    console.log("handling the route user 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 2");
+    // res.send("Hello, world 2!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 3");
+    // res.send("Hello, world 3!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 4");
+    res.send("Hello, world 4!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 5");
+    res.send("Hello, world 5!");
+    // next();
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- src/homework.md
+++ src/homework.md
@@ -18,3 +18,12 @@
 - Order of the routes matter a lot
 - Install Postman app and make a workspace/collection > test API call
 - Write logic to handle GET, POST, PATCH and DELETE API calls and test them on postman
+- Explore routing and use of ? , * , () in the routes
+- Use of regex in routes /a/
+- Reading the query params in the routes
+- Reading the dynamic routes - /user/:id
+
+- Multiple Route Handlers - play with the code
+- next()
+- next function and errors along with res.send()
+- app.use("/route, rH, rH2, [rH3, rH4], rH5);
